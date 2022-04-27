f2d0a448f51c59803d89c74a6c3c22ea755d067a
fancy <longlh@sphinx-software.com>
2022-04-27 15:18:29 +0700

    Add example for login with WalletConnect

--- user-wallet-sign/src/main.ts
+++ user-wallet-sign/src/main.ts
@@ -3,9 +3,26 @@ import { MintSDK } from '@kyuzan/mint-sdk-js'
 
 // --- configs
 const ACCESS_TOKEN = 'Set your Mint SDK Key'
+const INFURA_ID = 'Set your INFURA ID HERE'
 // ---
 
-const sdk = new MintSDK(ACCESS_TOKEN)
+const sdk = new MintSDK(ACCESS_TOKEN, {
+  selectWalletModal: {
+    cacheProvider: false,
+  },
+  providers: {
+    torus: {
+      options: {
+        showTorusButton: true,
+      },
+    },
+    walletconnect: {
+      options: {
+        infuraId: INFURA_ID,
+      }
+    }
+  },
+})
 
 const authButton = document.querySelector<HTMLButtonElement>('#submit')!
 authButton.addEventListener('click', async () => {
